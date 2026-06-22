1a21baaa58df077d82a172aa2585437b21914435
Leo <veursk@gmail.com>
2026-06-22 14:33:06 +0900

    test: add CrowdFundUnitTest with invalid target amount and deadline tests

---
+++ test/unit/CrowdFundUnitTest.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {ICrowdFunding} from "../../src/ICrowdFunding.sol";
+import {CrowdFunding} from "../../src/CrowdFunding.sol";
+import {DeployCrowdFunding} from "../../script/DeployCrowdFunding.s.sol";
+
+contract CrowdFundUnitTest is Test {
+    CrowdFunding crowdFunding;
+
+    function setUp() public {
+        DeployCrowdFunding deployer = new DeployCrowdFunding();
+        crowdFunding = deployer.run();
+    }
+
+    function testRevertInvalidTargetAmount() public {
+        vm.expectRevert(ICrowdFunding.CrowdFund__InvalidTargetAmount.selector);
+        crowdFunding.launch(0, 1000);
+    }
+
+    function testIfDeadLineNotFuture() public {
+        vm.warp(10000);
+        vm.expectRevert(ICrowdFunding.CrowdFund__DeadLineMustBeFuture.selector);
+        crowdFunding.launch(1 ether, block.timestamp - 1000);
+    }
+}
