677bcd46e1994974668ddd17b6cb0cd76502b3ef
Tokyo Night <6vwsbdjs27@privaterelay.appleid.com>
2026-06-29 16:00:50 +0900

    chore: use injected connectors

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -1,10 +1,12 @@
-import { createConfig, http } from "@wagmi/core";
+import { metaMask } from "@wagmi/connectors";
+import { createConfig, http, injected } from "@wagmi/core";
 import { sepolia, anvil } from "viem/chains";
 
 const DEFAULT_ANVIL_RPC_URL = "http://127.0.0.1:8545";
 
 export const config = createConfig({
   chains: [sepolia, anvil],
+  connectors: [injected()],
   ssr: true,
   transports: {
     [sepolia.id]: http(),
